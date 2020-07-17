2cfd68e91e5353d675956f004aa39b1531eae5cd
SubinAdhikari <subinadhikari61@gmail.com>
2020-07-17 16:08:07 +0545

    Updates

---
+++ precache-manifest.c2d86c407dcafd9b49aaa73855372641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1153c84e8e03f863b6cd71046506ab5c",
+    "url": "/weathertoday/index.html"
+  },
+  {
+    "revision": "9e82dcee6da47d94c1e1",
+    "url": "/weathertoday/static/css/main.800350bb.chunk.css"
+  },
+  {
+    "revision": "bbaa1eed93722eb415f3",
+    "url": "/weathertoday/static/js/2.ca73efcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weathertoday/static/js/2.ca73efcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e82dcee6da47d94c1e1",
+    "url": "/weathertoday/static/js/main.d411d987.chunk.js"
+  },
+  {
+    "revision": "3ea3ed9706df932b1843",
+    "url": "/weathertoday/static/js/runtime-main.7e1d19ce.js"
+  }
+]);
